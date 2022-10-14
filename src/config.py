59544362710de9b548940d3270904ad130fac061
saadmk11 <saad.mk112@gmail.com>
2022-10-14 21:02:14 +0600

    Refactor Code for rebase

---
+++ src/config.py
@@ -0,0 +1,92 @@
+import json
+from typing import Any, Mapping, NamedTuple
+
+import github_action_utils as gha_utils  # type: ignore
+
+
+class ActionEnvironment(NamedTuple):
+    repository: str
+    base_branch: str
+    event_name: str
+
+    @classmethod
+    def from_env(cls, env: Mapping[str, str]) -> "ActionEnvironment":
+        return cls(
+            repository=env["GITHUB_REPOSITORY"],
+            base_branch=env["GITHUB_REF"],
+            event_name=env["GITHUB_EVENT_NAME"],
+        )
+
+
+class Configuration(NamedTuple):
+    """Configuration class for GitHub Actions Version Updater"""
+
+    github_token: str | None = None
+    git_committer_username: str = "github-actions[bot]"
+    git_committer_email: str = "github-actions[bot]@users.noreply.github.com"
+    pull_request_title: str = "Update GitHub Action Versions"
+    commit_message: str = "Update GitHub Action Versions"
+    ignore_actions: set[str] = set()
+
+    @property
+    def git_commit_author(self) -> str:
+        """git_commit_author option"""
+        return f"{self.git_committer_username} <{self.git_committer_email}>"
+
+    @classmethod
+    def create(cls, env: Mapping[str, str | None]) -> "Configuration":
+        """
+        Create a Configuration object from environment variables
+        """
+        cleaned_user_config: dict[str, Any] = cls.clean_user_config(
+            cls.get_user_config(env)
+        )
+        return cls(**cleaned_user_config)
+
+    @classmethod
+    def get_user_config(cls, env: Mapping[str, str | None]) -> dict[str, str | None]:
+        """
+        Read user provided input and return user configuration
+        """
+        user_config: dict[str, str | None] = {
+            "github_token": env.get("INPUT_TOKEN"),
+            "git_committer_username": env.get("INPUT_COMMITTER_USERNAME"),
+            "git_committer_email": env.get("INPUT_COMMITTER_EMAIL"),
+            "pull_request_title": env.get("INPUT_PULL_REQUEST_TITLE"),
+            "commit_message": env.get("INPUT_COMMIT_MESSAGE"),
+            "ignore_actions": env.get("INPUT_TOKEN"),
+        }
+        return user_config
+
+    @classmethod
+    def clean_user_config(cls, user_config: dict[str, str | None]) -> dict[str, Any]:
+        cleaned_user_config: dict[str, Any] = {}
+
+        for key, value in user_config.items():
+            if key in cls._fields:
+                cleaned_value = getattr(cls, f"clean_{key.lower()}", lambda x: x)(value)
+
+                if cleaned_value is not None:
+                    cleaned_user_config[key] = cleaned_value
+
+        return cleaned_user_config
+
+    @staticmethod
+    def clean_ignore_actions(value: Any) -> set[str] | None:
+        if isinstance(value, str) and value.startswith("[") and value.endswith("]"):
+            ignore_actions = json.loads(value)
+
+            if isinstance(ignore_actions, list) and all(
+                isinstance(item, str) for item in ignore_actions
+            ):
+                return set(ignore_actions)
+            else:
+                gha_utils.error(
+                    "Invalid input for `ignore` field, "
+                    f"expected JSON array of strings but got `{value}`"
+                )
+                raise SystemExit(1)
+        elif isinstance(value, str):
+            return {s.strip() for s in value.split(",")}
+        else:
+            return None
---
+++ src/utils.py
@@ -0,0 +1,77 @@
+from functools import lru_cache
+
+import github_action_utils as gha_utils  # type: ignore
+import requests
+
+
+@lru_cache
+def get_request_headers(github_token: str | None = None) -> dict[str, str]:
+    """Get headers for GitHub API request"""
+    headers = {"Accept": "application/vnd.github.v3+json"}
+
+    if github_token:
+        headers.update({"authorization": f"Bearer {github_token}"})
+
+    return headers
+
+
+def create_pull_request(
+    pull_request_title: str,
+    repository_name: str,
+    base_branch_name: str,
+    head_branch_name: str,
+    body: str,
+    github_token: str | None = None,
+) -> None:
+    """Create pull request on GitHub"""
+    with gha_utils.group("Create Pull Request"):
+        url = f"https://api.github.com/repos/{repository_name}/pulls"
+        payload = {
+            "title": pull_request_title,
+            "head": head_branch_name,
+            "base": base_branch_name,
+            "body": body,
+        }
+
+        response = requests.post(
+            url, json=payload, headers=get_request_headers(github_token)
+        )
+
+        if response.status_code == 201:
+            html_url = response.json()["html_url"]
+            gha_utils.echo(f"Pull request opened at {html_url} \U0001F389")
+        else:
+            gha_utils.error(
+                f"Could not create a pull request on "
+                f"{repository_name}, status code: {response.status_code}"
+            )
+            raise SystemExit(1)
+
+
+def display_whats_new() -> None:
+    """function that prints what's new in GitHub Actions Version Updater Latest Version"""
+    url = "https://api.github.com/repos/saadmk11/github-actions-version-updater/releases/latest"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        response_data = response.json()
+        latest_release_tag = response_data["tag_name"]
+        latest_release_html_url = response_data["html_url"]
+        latest_release_body = response_data["body"]
+
+        group_title = f"\U0001F389 What's New In GitHub Actions Version Updater {latest_release_tag} \U0001F389"
+
+        with gha_utils.group(group_title):
+            gha_utils.echo(latest_release_body)
+            gha_utils.echo(
+                f"Get More Information about '{latest_release_tag}' "
+                f"Here: {latest_release_html_url}"
+            )
+            gha_utils.echo(
+                "\nTo use these features please upgrade to "
+                f"version '{latest_release_tag}' if you haven't already."
+            )
+            gha_utils.echo(
+                "\nReport Bugs or Add Feature Requests Here: "
+                "https://github.com/saadmk11/github-actions-version-updater/issues"
+            )
